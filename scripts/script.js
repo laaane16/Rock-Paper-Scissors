0ab0a9f32c609d10e21dc8c42eeb5bc6862a0161
laaane16 <nikon200402@gmail.com>
2024-07-19 17:12:07 +0300

    Game creation process

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,34 @@
 (function(){
-    
+    class Game{
 
+        constructor(name, gameLogic){
+            this.name = name;
+            this.gameLogic = gameLogic;
+        }
+
+    }
+
+    function getComputerChoice(){
+        return Math.floor(Math.random() * 3);
+    }
+
+    function gameLogic(fn){
+        if (fn() == 1){}
+    }
+
+    class Moves{
+
+        constructor(...arg){
+            this.moves = [arg] 
+        }
+
+    }
+
+    let rockPaperScissors = new Game('ROCK-PAPER-SCISSORS', gameLogic(getComputerChoice))
+
+    let movesForRPS = new Moves('paper', 'scissors', 'rock');
+
+    console.log(movesForRPS.moves)
     addEventListener('DOMContentLoaded', function(){
 
     })
